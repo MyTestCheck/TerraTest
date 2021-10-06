dd43015b6e24fea587d520309260e3cd2889faf9
MyTestCheck <91868040+MyTestCheck@users.noreply.github.com>
2021-10-06 09:53:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-resource "google_storage_bucket" "bucket" {
-  name = "test-bucket-random-001122"
+resource "google_bigquery_dataset" "default" {
+  dataset_id                  = "teracheck1"
+  project                     = "terratest-328102"
+  friendly_name               = "people"
+  description                 = "Person informations"
+  location                    = "US"
 }
-
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-001123"
-}
-
