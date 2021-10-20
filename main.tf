7b3cd6f39219b11c8b8affc25684ab23327dc758
Pascoal Dias <sunny.pascoal.dias@accenture.com>
2021-10-20 10:04:49 +0530

    dataset1 creation

--- main.tf
+++ main.tf
@@ -17,3 +17,11 @@ resource "google_storage_bucket" "bucket4" {
 resource "google_storage_bucket" "bucket5" {
   name = "terratestfive"
 }
+
+resource "google_bigquery_dataset" "dataset1" {
+  dataset_id                  = "dataset1"
+  project                     = "terratest-328102"
+  friendly_name               = "dataset1"
+  description                 = "dataset1 from terraform"
+  location                    = "US"
+}
