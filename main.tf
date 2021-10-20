fab51fb147469afd12de687b81b2db86201345d2
MyTestCheck <91868040+MyTestCheck@users.noreply.github.com>
2021-10-20 12:13:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,13 @@ resource "google_storage_bucket" "bucket6" {
   name = "terratestsix"
 }
 
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "example_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
