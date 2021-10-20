8532ae646980c40a0933c757861726b59329ccae
MyTestCheck <91868040+MyTestCheck@users.noreply.github.com>
2021-10-20 11:24:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,9 +23,4 @@ resource "google_bigquery_dataset" "default" {
   friendly_name               = "test"
   description                 = "This is a test description"
   location                    = "EU"
-  default_table_expiration_ms = 3600000
-
-  labels {
-    env = "default"
-  }
 }
