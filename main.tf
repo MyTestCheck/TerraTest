41e4b4a8e1f58dd2a4e06b62f510ae02dbb27b0d
Pascoal Dias <sunny.pascoal.dias@accenture.com>
2021-10-20 10:08:47 +0530

    changes

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ resource "google_storage_bucket" "bucket5" {
   name = "terratestfive"
 }
 
-resource "google_bigquery_dataset" "dataset1" {
+resource "google_bigquery_dataset" "dataset1test" {
   dataset_id                  = "dataset1"
   project                     = "terratest-328102"
-  friendly_name               = "dataset1"
+  friendly_name               = "dataset1test"
   description                 = "dataset1 from terraform"
   location                    = "US"
 }
