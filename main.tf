c647c464073f46818af45ede5363f84ffa2568da
MyTestCheck <91868040+MyTestCheck@users.noreply.github.com>
2021-10-23 11:42:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,10 @@ resource "google_storage_bucket" "bucket2" {
 
 resource "google_bigquery_dataset" "dataset1" {
   dataset_id                  = "dataset_one"
-  friendly_name               = "test"
-  description                 = "This is a test description"
-  location                    = "EU"
-  default_table_expiration_ms = 3600000
-  delete_contents_on_destroy  = 1
+#   friendly_name               = "test"
+#   description                 = "This is a test description"
+#   location                    = "EU"
+#   default_table_expiration_ms = 3600000
   
   labels = {
    env = "default"
