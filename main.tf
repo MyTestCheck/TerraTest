ea6d783fc705731484fe648702c325a8302be1e4
MyTestCheck <91868040+MyTestCheck@users.noreply.github.com>
2021-10-23 10:49:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,40 +22,8 @@ resource "google_bigquery_dataset" "dataset" {
   }
 }
 
-resource "google_bigquery_dataset" "dataset1" {
-  dataset_id                  = "example_dataset1"
-  friendly_name               = "test1"
-  description                 = "This is a test description"
-  location                    = "EU"
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    env = "default"
-  }
-}
-
-resource "google_bigquery_dataset" "dataset2" {
-  dataset_id                  = "example_dataset2"
-  friendly_name               = "test2"
-  description                 = "This is a test description"
-  location                    = "EU"
-  default_table_expiration_ms = "null"
-  
-  labels = {
-    env = "default"
-  }
-}
-
-
-
-
-
-
 
 #.............................................Tables............................................
 
-resource "google_bigquery_table" "default" {
-  dataset_id = "example_dataset1"
-  table_id   = "example_table1"
-}
+
 
