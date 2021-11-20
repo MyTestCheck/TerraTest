707513adbfc132ccb089963617470580538c3230
MyTestCheck <91868040+MyTestCheck@users.noreply.github.com>
2021-11-20 13:13:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,39 +5,39 @@ resource "google_storage_bucket" "bucket" {
   location = "us"
 }
 
-resource "google_storage_bucket" "bucket2" {
-  name = "terratesttwo"
-  location = "us"
-}
+# resource "google_storage_bucket" "bucket2" {
+#   name = "terratesttwo"
+#   location = "us"
+# }
 
-#...................................Datasets....................................................
+# #...................................Datasets....................................................
 
-resource "google_bigquery_dataset" "dataset1" {
-  dataset_id = "dataset_one"
-}
+# resource "google_bigquery_dataset" "dataset1" {
+#   dataset_id = "dataset_one"
+# }
 
-resource "google_bigquery_dataset" "dataset2" {
-  dataset_id = "dataset_two"
-}
+# resource "google_bigquery_dataset" "dataset2" {
+#   dataset_id = "dataset_two"
+# }
 
-resource "google_bigquery_dataset" "dataset3" {
-  dataset_id = "dataset_three"
-  delete_contents_on_destroy = false
-}
+# resource "google_bigquery_dataset" "dataset3" {
+#   dataset_id = "dataset_three"
+#   delete_contents_on_destroy = false
+# }
 
 
-#....................................Tables............................................
+# #....................................Tables............................................
 
-resource "google_bigquery_table" "table1" {
-  dataset_id = "dataset_one"
-  table_id   = "table_one"
-  deletion_protection = true
-}
+# resource "google_bigquery_table" "table1" {
+#   dataset_id = "dataset_one"
+#   table_id   = "table_one"
+#   deletion_protection = true
+# }
 
-resource "google_bigquery_table" "table2" {
-  dataset_id = "dataset_three"
-  table_id   = "table_two"
-  deletion_protection = true
-}
+# resource "google_bigquery_table" "table2" {
+#   dataset_id = "dataset_three"
+#   table_id   = "table_two"
+#   deletion_protection = true
+# }
 
 
