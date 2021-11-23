9a0326f2fb78d049078e6a595f978ec5ab7c1384
MyTestCheck <test8149833010@gmail.com>
2021-11-23 21:34:18 +0530

    vm-1

--- main.tf
+++ main.tf
@@ -19,3 +19,20 @@ resource "google_bigquery_table" "tables" {
    table_id   = each.value.table_id
 }
 
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
