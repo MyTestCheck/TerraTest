90222e75ba9da8c429789df3360413521828522a
MyTestCheck <91868040+MyTestCheck@users.noreply.github.com>
2022-01-28 13:03:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -41,8 +41,8 @@ variable "db_instances" {
     region = string
     database_version = string
     tier = string
-    day = number
-    hour = number
+    day = string
+    hour = string
     deletion_protection = string
   }))
   default = []
